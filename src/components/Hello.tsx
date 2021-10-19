590fbb7baa04d960d59d18c00489b8922c228ded
Steven Chavarria <steven.chavarria4@gmail.com>
2021-10-18 21:05:11 -0600

    added styles

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface HelloProps {
 	name: string;
@@ -11,13 +12,18 @@ interface HelloProps {
 export const Hello: React.FC<HelloProps> = ({ name, age, address = 'no address', email, handleClick }) => {
 
 
-	return <div>
+	return <Container>
 		<ul onClick={()=> handleClick(name)}>
 			<li>Name: {name}</li>
 			<li>Age: {age}</li>
 			<li>Address: {address}</li>
 			<li> Email: {email}</li>
 		</ul>
+	</Container>
+};
 
-	</div>
-}
+const Container = styled.div`
+	width:200px;
+	padding: 15px 10px;
+	background-color: blue;
+`;
